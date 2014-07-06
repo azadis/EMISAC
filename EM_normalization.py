1f9d76ae984981b33c7d33e85af14f66f35c60d7
Samaneh Azadi <sam.ai_156@yahoo.com>
2014-07-06 15:45:46 -0700

    Main code

--- EM_normalization.py
+++ EM_normalization.py
@@ -2,6 +2,22 @@
 
 "This code is developed by Samaneh Azadi in Prof. Pieter Abbeel's lab at UC Berkeley with the colaboration of Jeremy Maitin-Shepard."
 
+"The main code"
+
+""" Copyright (C) {2014}  {Samaneh Azadi, Jeremy Maitin-Shepard, Pieter Abbeel}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
 import os
 import sys
 import getopt
@@ -287,7 +303,7 @@ def minimizer_downsampled(partnum,step):
 	da.close()
 	img_path='%s/img_partition%s.h5'%(output_path,partnum)
 	"""call for the main normalization function--------------"""
-	os.system("./normalize_partition_fastDwnSample_final_ver.py --grp %s --lm %f --beta_bnd_l %f --beta_bnd_u %f --alpha_bnd_l %f --alpha_bnd_u %f --cnst %f --fact %f --beta_val_l %f --beta_val_u %f --alpha_val_l %f --alpha_val_u %f %s %d %d %d %d %d %d %d %s"%(group_name,lmbda,lower_bnd_beta,upper_bnd_beta,lower_bnd_alpha,upper_bnd_alpha,cnst,fact,lower_val_beta,upper_val_beta,lower_val_alpha,upper_val_alpha,img_path,partnum,window_size_x/step_x,window_size_y/step_y,num_partitions_x,num_partitions_y,num_parallel,step,output_path))
+	os.system("./normalize_partition.py --grp %s --lm %f --beta_bnd_l %f --beta_bnd_u %f --alpha_bnd_l %f --alpha_bnd_u %f --cnst %f --fact %f --beta_val_l %f --beta_val_u %f --alpha_val_l %f --alpha_val_u %f %s %d %d %d %d %d %d %d %s"%(group_name,lmbda,lower_bnd_beta,upper_bnd_beta,lower_bnd_alpha,upper_bnd_alpha,cnst,fact,lower_val_beta,upper_val_beta,lower_val_alpha,upper_val_alpha,img_path,partnum,window_size_x/step_x,window_size_y/step_y,num_partitions_x,num_partitions_y,num_parallel,step,output_path))
 	
 #----------------------------------------------------------	
 def minimizer(partnum,step):
@@ -298,7 +314,7 @@ def minimizer(partnum,step):
 	da.close()
 	img_path='%s/img_partition%s.h5'%(output_path,partnum)
 	"""call for the main normalization function--------------"""
-	os.system("./normalize_partition_fastDwnSample_final_ver.py --grp %s --lm %f --beta_bnd_l %f --beta_bnd_u %f --alpha_bnd_l %f --alpha_bnd_u %f --cnst %f --fact %f --beta_val_l %f --beta_val_u %f --alpha_val_l %f --alpha_val_u %f %s %d %d %d %d %d %d %d %s"%(group_name,lmbda,lower_bnd_beta,upper_bnd_beta,lower_bnd_alpha,upper_bnd_alpha,cnst,fact,lower_val_beta,upper_val_beta,lower_val_alpha,upper_val_alpha,img_path,partnum,window_size_x,window_size_y,num_partitions_x,num_partitions_y,num_parallel,step,output_path))
+	os.system("./normalize_partition.py --grp %s --lm %f --beta_bnd_l %f --beta_bnd_u %f --alpha_bnd_l %f --alpha_bnd_u %f --cnst %f --fact %f --beta_val_l %f --beta_val_u %f --alpha_val_l %f --alpha_val_u %f %s %d %d %d %d %d %d %d %s"%(group_name,lmbda,lower_bnd_beta,upper_bnd_beta,lower_bnd_alpha,upper_bnd_alpha,cnst,fact,lower_val_beta,upper_val_beta,lower_val_alpha,upper_val_alpha,img_path,partnum,window_size_x,window_size_y,num_partitions_x,num_partitions_y,num_parallel,step,output_path))
 #----------------------------------------------------------			
 		
 	
