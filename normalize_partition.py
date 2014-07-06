04cb8767317609a9ec0a3efeb85570a5d1bd2552
Samaneh Azadi <sam.ai_156@yahoo.com>
2014-07-06 15:47:39 -0700

    nested file

--- normalize_partition.py
+++ normalize_partition.py
@@ -2,6 +2,20 @@
 
 "This code is developed by Samaneh Azadi in Prof. Pieter Abbeel's lab at UC Berkeley with the colaboration of Jeremy Maitin-Shepard."
 
+""" Copyright (C) {2014}  {Samaneh Azadi, Jeremy Maitin-Shepard, Pieter Abbeel}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
 import numpy as np
 import scipy as sp
 import getopt
